a0a6a312b13b3080f6892e218942a5ee0c9cf4cd
AndoSama <aaeg3006@gmail.com>
2024-11-04 01:38:24 -0300

    Se corrige alineado fuera de la lista con los mensajes de productos no encontrados

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -3,15 +3,18 @@ import { ItemCard } from '../ItemCard/ItemCard';
 import PropTypes from 'prop-types';
 
 export const ItemList = ({ items = [], onClick }) => (
-  <ul className="item-list">
+  <>
+    <ul className="item-list">
     {items.map((item) => (
       <li key={item.id} className="item-list__item">
         <ItemCard item={item} onClick={onClick} />
       </li>
     ))}
+    </ul>
     {items.length === 0 && <p className="item-list__message">No se encontraron productos</p>}
     {!!items.length && <p className="item-list__message">No hay mas productos para mostrar...</p>}
-  </ul>
+  </>
+  
 );
 
 ItemList.propTypes = {
